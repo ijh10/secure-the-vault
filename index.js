6b778627af11b71064dd999e25c04229e09d5039
“IzzyHunter” <izzy@sensi.fit>
2025-01-22 18:16:29 -0500

    finished vault

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log ("hello world")
+let code1 ;
+code1 = 7 + 3;
+const code2 = 400 / 10 - 0;
+const code3 = 100 % 61
+console.log (code3)
+const message = "The vault has been secured. The combination is:"
+console.log (message)
+const codeA = code1 + "-" + code2 + "-" + code3 ;
+const codeB = `${code1} - ${code2} - ${code3}`
+console.log (codeB)
+console.log (message, codeA, codeB)
